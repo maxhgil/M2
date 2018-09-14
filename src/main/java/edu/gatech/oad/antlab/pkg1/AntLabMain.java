5ced74f93c8cee3c79665ce537143fc205d7bea3
jmagi98 <jmaginnes@gmail.com>
2018-09-14 10:25:56 -0400

    added param in main method comments so javadoc would run

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -84,6 +84,7 @@ import edu.gatech.oad.antlab.pkg2.*;
     
     /**
      * entry point for the program
+     * @param args string of arugments passed to command line
      */
      public static void main(String[] args) {
         
